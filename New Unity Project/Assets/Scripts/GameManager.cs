7426ea8b8f3fb025b424758443b2a712a6d515c6
Amanda Souza Carvalho <amanda.carvalho@academico.ifrn.edu.br>
2022-10-10 12:02:13 -0300

    aula 10/10

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance; // Instancia do singleton 
+    
     [SerializeField] 
     private string guiName; // nome da fase de interface 
 
@@ -16,12 +19,48 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private GameObject playerAndCameraPrefab; //referencia para a prefab do jogador + camera
-    
+
+    private void Awake()
+    {
+        // Condição de criação do singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            
+            // Impede o objeto indicado entre parenteses seja destruído
+            DontDestroyOnLoad(this.gameObject); // Referencia para o objeto que3 contém o gamemanager
+        }
+        else Destroy(this.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        // Impede o objeto indicado entre parenteses seja destruído
-        DontDestroyOnLoad(this.gameObject); // Referencia para o objeto que3 contém o gamemanager
+        // Se estiver iniciando a cena a partir de Initialization, carregue o jogo 
+        // do jeito de antes
+        if (SceneManager.GetActiveScene().name == "Initialization")
+            StartGameFromInitialization();
+        else // caso contrário, esta iniciando a partir do level carregue o jogo de modo apropriado
+            StartGameFromLevel();
+    }
+
+    private void StartGameFromLevel()
+    {
+        // 1 - carrega a cena da interface de modo adtivo para não apagar a cena do level 
+        // da memória ram
+        SceneManager.LoadScene(guiName, LoadSceneMode.Additive);
+        
+        // 2 - precisa instanciar o jogador na cena 
+        // Começa procurando o objeto PlayerStart na cena do level  
+        Vector3 playerStartPosition = GameObject.Find("PlayerStart").transform.position;
+
+        //instancia o prefab do jogador na posição do player start com rotação zerada
+        Instantiate(playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
+    }
+    
+    private void StartGameFromInitialization()
+    {
+        
         // 1 - carregar a cena de interface e do jogo 
         SceneManager.LoadScene(guiName);
         //SceneManager.LoadScene(levelName, LoadSceneMode.Additive); // Additive carrega uma nova cena sem descarregar a anterior
@@ -58,10 +97,8 @@ public class GameManager : MonoBehaviour
             Instantiate(playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
 
             // 3 - começar a partida 
-        };
-
+        };        
     }
-    
     
     
 }
